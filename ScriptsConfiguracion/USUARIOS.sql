731e4e282f017679c20ec1a9f7d9307b45f8fafe
lmmv <sakythelion@gmail.com>
2020-06-12 19:01:07 +0200

    Esquemas correctos.

--- ScriptsConfiguracion/USUARIOS.sql
+++ ScriptsConfiguracion/USUARIOS.sql
@@ -1,17 +1,20 @@
 /*CREACION DE USUARIOS.*/
-DROP USER IF EXISTS `adminbd`;
-CREATE USER 'adminbd'@'localhost' IDENTIFIED BY '2020_adminbd';
-GRANT ALL_PRIVILEGES ON *.* TO 'adminbd'@'localhost';
+START TRANSACTION;
 
-DROP USER IF EXISTS `ima_user`;
-CREATE USER 'ima_user'@'localhost' IDENTIFIED BY 'ima_user_2020';
-GRANT ALL_PRIVILEGES ON bam.* TO 'ima_user'@'localhost';
-GRANT ALL_PRIVILEGES ON pcm.* TO 'ima_user'@'localhost';
-GRANT ALL_PRIVILEGES ON wop.* TO 'ima_user'@'localhost';
+DROP USER IF EXISTS 'admindb_ima'@'localhost';	
+CREATE USER `admindb_ima`@`localhost` IDENTIFIED BY 'admindb2020' PASSWORD EXPIRE NEVER;
+GRANT ALL ON *.* TO 'adminbd_ima'@'localhost';
+
+DROP USER IF EXISTS 'ima_user'@'localhost';	
+CREATE USER `ima_user`@`localhost` IDENTIFIED BY 'ima_user_2020' PASSWORD EXPIRE NEVER;
+GRANT ALL ON bam.* TO 'ima_user'@'localhost';
+GRANT ALL ON pcm.* TO 'ima_user'@'localhost';
+GRANT ALL ON wop.* TO 'ima_user'@'localhost';
 
 
-DROP USER IF EXISTS `invitado`;
-CREATE USER 'invitado'@'localhost' IDENTIFIED BY 'invitado';
-GRANT SELECT ON bam.* TO 'invitado'@'localhost';
-GRANT SELECT ON pcm.* TO 'invitado'@'localhost';
-GRANT SELECT ON wop.* TO 'invitado'@'localhost';
+DROP USER IF EXISTS 'invitado'@'localhost';	
+CREATE USER `invitado`@`localhost` IDENTIFIED BY 'invitado' PASSWORD EXPIRE NEVER;
+GRANT SELECT ON bam.* TO `invitado`@`localhost`;
+GRANT SELECT ON pcm.* TO `invitado`@`localhost`;
+GRANT SELECT ON wop.* TO `invitado`@`localhost`;
+COMMIT;
